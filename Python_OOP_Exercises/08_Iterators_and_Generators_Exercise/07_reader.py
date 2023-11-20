586ff0b2027acdd38d50629434cb6a6ce9c987e6
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-11-20 19:18:26 +0100

    Update 07_reader.py

--- Python_OOP_Exercises/08_Iterators_and_Generators_Exercise/07_reader.py
+++ Python_OOP_Exercises/08_Iterators_and_Generators_Exercise/07_reader.py
@@ -1,7 +1,8 @@
 def read_next(*args):
     for iterable in args:
-        for el in iterable:
-            yield el
+        yield from iterable
+        # for el in iterable:
+        #     yield el
 
 
 # test code
