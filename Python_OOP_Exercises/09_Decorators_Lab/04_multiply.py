9e61a8f70a9d772d613490887c7f99f06df85391
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-11-22 11:14:54 +0100

    Add files via upload

---
+++ Python_OOP_Exercises/09_Decorators_Lab/04_multiply.py
@@ -0,0 +1,24 @@
+def multiply(times):
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            res = function(*args, **kwargs)
+            result = res * times
+            return result
+        return wrapper
+    return decorator
+
+
+@multiply(3)
+def add_ten(number):
+    return number + 10
+
+
+print(add_ten(3))
+
+
+@multiply(5)
+def add_ten(number):
+    return number + 10
+
+
+print(add_ten(6))
