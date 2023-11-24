f0d58c8eb4ddcef9a29b63726848117e75d5596f
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-11-24 10:30:22 +0100

    Add files via upload

---
+++ Python_OOP_Exercises/09_Decorators_Exercise/06_html_tags.py
@@ -0,0 +1,25 @@
+def tags(tag):
+    def decorator(func):
+        def wrapper(*args):
+            start_tag = f"<{tag}>"
+            func(*args)
+            end_tag = f"</{tag}>"
+            return f"{start_tag}{func(*args)}{end_tag}"
+        return wrapper
+    return decorator
+
+
+@tags('p')
+def join_strings(*args):
+    return "".join(args)
+
+
+print(join_strings("Hello", " you!"))
+
+
+@tags('h1')
+def to_upper(text):
+    return text.upper()
+
+
+print(to_upper('hello'))
